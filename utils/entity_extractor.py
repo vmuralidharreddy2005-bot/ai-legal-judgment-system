fd84621efcbca690a86e2e977be79e3789c1df7c
Muralidhar Reddy <vmuralidharreddy2005@gmail.com>
2026-05-11 00:00:50 +0530

    Removed spacy dependency

--- utils/entity_extractor.py
+++ utils/entity_extractor.py
@@ -1,16 +1,13 @@
-import spacy
-
-# Load spaCy model
-nlp = spacy.load("en_core_web_sm")
-
 def extract_entities(text):
 
-    doc = nlp(text)
+    words = text.split()
 
     entities = []
 
-    for ent in doc.ents:
+    for word in words:
 
-        entities.append((ent.text, ent.label_))
+        if word.istitle():
 
-    return entities
+            entities.append(word)
+
+    return list(set(entities))
